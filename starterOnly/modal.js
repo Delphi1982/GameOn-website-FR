cc8cd6b9574b05f6fd60e5abce31d1143972e238
Delphine Josserand <delphine.josserand@gmail.com>
2024-05-23 12:52:45 +0200

    ajout formuaire validate

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -35,5 +35,49 @@ function collectCivilData() {
 
 }
 
+// validation du formulaire
+function validate() {
+  let isValid = true;
+
+  // validation du prénom
+  const firstName = document.getElementById("first").value; // Accéder à la valeur du champ
+  if (firstName.length < 2) {
+    isValid = false;
+    console.log("Erreur: Veuillez entrer 2 caractères ou plus pour le champ du prénom.");
+  }
+
+  // validation du nom
+  const lastName = document.getElementById("last").value; 
+  if (lastName.length < 2) {
+    isValid = false;
+    console.log("Erreur: Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+  }
+
+  // validation de l'email
+  const email = document.getElementById("email").value; 
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+  if (!emailRegex.test(email)) {
+    isValid = false;
+    console.log("Erreur: Veuillez entrer une adresse email valide.");
+  }
+
+  // validation de la date de naissance
+  const birthdate = document.getElementById("birthdate").value; 
+  if (!birthdate) {
+    isValid = false;
+    console.log("Erreur: Veuillez entrer votre date de naissance.");
+  }
+
+  // validation de la quantité
+  const quantity = document.getElementById("quantity").value; 
+  if (quantity === "" || quantity < 0 || quantity > 99) {
+    isValid = false;
+    console.log("Erreur: Veuillez entrer une quantité valide (entre 0 et 99).");
+  }
+
+  return isValid;
+}
+
+
 
 
